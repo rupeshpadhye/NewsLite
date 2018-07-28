df5126b268849e87b995467820d915e9d5ca1870
Rupesh <ruepshpadhye@gmail.com>
2018-07-28 17:12:12 +0530

    removed details

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -12,43 +12,31 @@ const InlineStyleObj = {
     imageblock: {
         'display':'block',
         'margin':'auto',
+    },
+    margin: {
+        'margin': '5px'
     }
 }
 
 
 const SettingsPage = (props) => (
     <React.Fragment>
-            <NewsFeederHeader>
-                <NewsFeedSettingIcon  goTo="/" shouldHide={false} icon='keyboard_backspace' ></NewsFeedSettingIcon >
-                <HeaderTitle header="About" />  
-            </NewsFeederHeader>
-          <div className="about">  
-           <h1>News Lite</h1>
-           <div>News Lite is React framework based Progressive web app. News feed provided by <b>NEWSAPI.org</b> </div>
-           
-           <div style={InlineStyleObj.imageblock}>
-           <img style={InlineStyleObj.image} alt="" src="https://avatars2.githubusercontent.com/u/10848048?s=196&amp;v=4" width="196" />
-           <h3>Rupesh Padhye (Developer)</h3>
-           </div>
-            <div>
-            <a href="https://github.com/rupeshpadhye" target="_blank" rel="noopener">GitHub</a>
-            <a href="https://www.linkedin.com/in/rupesh-padhye-16301417/" target="_blank" rel="noopener">Linkedin</a>
+        <NewsFeederHeader>
+            <NewsFeedSettingIcon  goTo="/" shouldHide={false} icon='keyboard_backspace' ></NewsFeedSettingIcon >
+            <HeaderTitle header="About" />  
+         </NewsFeederHeader>
+         <div className="about">  
+            <h1>News Lite</h1>
+            <div>News Lite is React framework based Progressive web app. News feed provided by <b>NEWSAPI.org</b> </div>
+            <div style={InlineStyleObj.imageblock}>
+                <img style={InlineStyleObj.image} alt="" src="https://avatars2.githubusercontent.com/u/10848048?s=196&amp;v=4" width="196" />
+                <h3>Rupesh Padhye (Developer)</h3>
             </div>
-            <div>
-              <h4>Technical skills</h4>
-              <ui>
-                <li>UI Technologies - Javascript , React ,Polymer Js ,Vue.js </li>
-                <li>Backend Technologies - Java, Spring ,Spring boot</li>
-                <li>Mobile Technologies - Android , Hybrid App -ionic 1.x ,</li>
-              </ui>
-            </div>
-            <div>
-              <h4>Contacts</h4>
-             <a href="https://github.com/rupeshpadhye" target="_blank" rel="noopener">GitHub</a>
-              
-              <a href="https://www.linkedin.com/in/rupesh-padhye-16301417/" target="_blank" rel="noopener">Linkedin</a>
-            </div>
-           </div> 
+        <div>
+             <a className="margin" href="https://github.com/rupeshpadhye" target="_blank" rel="noopener">GitHub</a>
+             <a className="margin" href="https://www.linkedin.com/in/rupesh-padhye-16301417/" target="_blank" rel="noopener">Linkedin</a>
+        </div>
+         </div>
     </React.Fragment>
 );
 
