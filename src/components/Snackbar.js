1426e096a019db892637c23160661fad7be33fd5
Rupesh <ruepshpadhye@gmail.com>
2018-07-30 22:17:31 +0530

    added snackbar test

--- src/components/Snackbar.js
+++ src/components/Snackbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import PropTypes from 'prop-types';
 
-class Snackbar extends Component {
-    render() {
-        return this.props.visible && <div className="snackbar">{this.props.message}</div>
-    }
+
+const Snackbar = (props) => props.visible && <div className="snackbar">{props.message}</div>
+
+Snackbar.propTypes = {
+    visible: PropTypes.string.isRequired,
+    message:PropTypes.string.isRequired,
 }
+
 
 const mapStateToProps = state => ({
     message: state.snackbar_message.message,
